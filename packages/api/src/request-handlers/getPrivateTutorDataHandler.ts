1280de55a06408f2e613991f78ee893a0c075296
Eli <13420359+eliasm307@users.noreply.github.com>
2021-04-15 06:21:05 +0100

    deleted old comments

--- packages/api/src/request-handlers/getPrivateTutorDataHandler.ts
+++ packages/api/src/request-handlers/getPrivateTutorDataHandler.ts
@@ -20,41 +20,6 @@ const handler: ApiGetPrivateStudentDataHandler = async (_, context) => {
   return {
     data,
   };
-
-  /*
-  // check if tutor already exists for this user
-  const docSnapshot = await firestore.doc(documentPath).get();
-
-  if (!docSnapshot.exists)
-    throw new functionsHttps.HttpsError(
-      "not-found",
-      "Could not read data because a student profile doesnt exist for this user, create one first"
-    );
-
-  // read data
-  try {
-    // const newSnapshot = await docSnapshot.ref.get();
-
-    const data = docSnapshot.data();
-
-    if (!isPrivateStudentData(data))
-      throw new functionsHttps.HttpsError(
-        "internal",
-        "Student data is not in the right format"
-      );
-
-    return {
-      success: true,
-      data,
-    };
-  } catch (error) {
-    throw new functionsHttps.HttpsError(
-      "internal",
-      "There was an issue reading the student data",
-      JSON.stringify(error)
-    );
-  }
-  */
 };
 
 export default handler;
--- packages/api/src/request-handlers/getStudentsBySubjectsHandler.ts
+++ packages/api/src/request-handlers/getStudentsBySubjectsHandler.ts
@@ -2,7 +2,7 @@ import { PublicStudentData } from '@adopt-a-student/common';
 
 import { ApiGetStudentsBySubjectsHandler } from '../declarations/interfaces';
 import extractPublicStudentData from '../utils/extractPublicStudentData';
-import { functionsHttps } from '../utils/firebase-admin';
+import { firestore, functionsHttps } from '../utils/firebase-admin';
 import getUsersBySubjects from '../utils/getUsersBySubjects';
 import verifyRequest from '../utils/verifyRequest';
 
@@ -20,51 +20,8 @@ const handler: ApiGetStudentsBySubjectsHandler = async (data, context) => {
     localeSubjectIds: data.localeSubjectIds,
     publicDataExtractor: (data) => extractPublicStudentData(data),
     userType: "Student",
+    firestore,
   });
-
-  /* array-contains-any is limited to 10 values, so split this into multiple requests if necessary
-    https://firebase.google.com/docs/firestore/query-data/queries#array-contains-any
-   */
-  /*
-  const groupedLocaleSubjectIds = groupArrayItems(data.localeSubjectIds, 10);
-
-  const subjectsField: keyof PrivateStudentData = "relatedSubjects";
-
-  const filteredStudentsPromises = groupedLocaleSubjectIds.map(
-    (subjectIdGroup) => {
-      return firestore
-        .collection(STUDENTS_COLLECTION_NAME)
-        .where(subjectsField, "array-contains-any", subjectIdGroup)
-        .get();
-    }
-  );
-
-  try {
-    // resolve promises in parallel
-    const filteredStudentGroupsResults = await Promise.all(
-      filteredStudentsPromises
-    );
-
-    // process and return public student data
-    return {
-      data: filteredStudentGroupsResults
-        // reduce to flat list
-        .reduce((accumulatedData, currentGroup) => {
-          const groupData = currentGroup.docs.map((doc) => doc.data());
-          return [...accumulatedData, ...groupData];
-        }, [] as any[])
-
-        // extract public data for each user
-        .map((data) => extractPublicTutorData(data)),
-    };
-  } catch (error) {
-    throw new functionsHttps.HttpsError(
-      "internal",
-      "There was an issue reading data from firestore",
-      JSON.stringify({ error })
-    );
-  }
-  */
 };
 
 export default handler;
