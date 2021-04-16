76c967ee29664ec47641b8bd38a6db538d3ee2c7
Eli <13420359+eliasm307@users.noreply.github.com>
2021-04-16 06:07:40 +0100

    moved api.write-subject-category-data to done

--- packages/api/src/declarations/interfaces/index.ts
+++ packages/api/src/declarations/interfaces/index.ts
@@ -40,7 +40,7 @@ export * from "./api/api.read-user-data";
 export * from "./api-tbc/api.write-relationships";
 export * from "./api-tbc/api.write-subject-data";
 export * from "./api/api.write-user-data";
-export * from "./api-tbc/api.write-subject-category-data";
+export * from "./api/api.write-subject-category-data";
 export * from "./api/api.read-subject-category-data";
 
 export type FirestoreAdmin = FirebaseFirestore.Firestore;
