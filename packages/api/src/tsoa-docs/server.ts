e43940c3fa1ac99f0a722b65d4e5c5dc4f052c00
Eli <13420359+eliasm307@users.noreply.github.com>
2021-04-17 20:52:44 +0100

    build scripts working

--- packages/api/src/tsoa-docs/server.ts
+++ packages/api/src/tsoa-docs/server.ts
@@ -3,5 +3,5 @@ import { app } from './app';
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`TSOA Docs app listening at http://localhost:${port}`)
 );
